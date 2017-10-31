c3ee14ad092954c2cdbce1401adb44e3f76ede09
Manuel Uberti <manuel@7bridges.eu>
2017-10-31 16:48:43 +0100

    Retrieve primary key column dynamically

--- src/remys/api/handlers.clj
+++ src/remys/api/handlers.clj
@@ -3,10 +3,44 @@
             [remys.services.mysql :as db]
             [ring.util.http-response :as response]))
 
-(api/defapi apis
-  {:coercion nil}
+(defn tables
+  "Get a sequence of all the tables in the database."
+  []
+  (keys @db/schema))
 
+(defn table-exists?
+  "Check if `t` exists in the database."
+  [t]
+  (->> (tables)
+       (some #{t})
+       nil?
+       not))
+
+(defn primary-key
+  "Find the primary key of the table `t`."
+  [t]
+  (->> (get @db/schema t)
+       (filter #(= (:column-key %) "PRI"))
+       first
+       :column-name))
+
+(defn query
+  ([table]
+   (-> (str "select * from " table)
+       (db/query!)))
+  ([table id]
+   (let [pk (primary-key table)]
+     (-> (str "select * from " table " where " pk " = " id)
+         (db/query!)))))
+
+(api/defapi apis
   (api/context
    "/api" [table id]
    (api/GET "/:table" [table]
-            (response/ok (db/query "select * from test")))))
+            (if (table-exists? table)
+              (response/ok (query table))
+              (response/not-found {:msg "Table not found"})))
+   (api/GET "/:table/:id" [table id]
+            (if (table-exists? table)
+              (response/ok (query table id))
+              (response/not-found {:msg "Table not found"})))))

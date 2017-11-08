e174bacf2556063abf3685e8f466215104b8fa6c
Manuel Uberti <manuel@7bridges.eu>
2017-11-08 17:38:33 +0100

    Add format utilities and unit tests

---
+++ src/remys/api/resources/format.clj
@@ -0,0 +1,86 @@
+(ns remys.api.resources.format
+  (:require [clojure.string :as string]
+            [remys.services.mysql :as db]))
+
+(defn wrap-string
+  "Wrap `s` in single quotes if `s` is a string. Useful for SQL."
+  [s]
+  (if (string? s)
+    (str "'" s "'")
+    s))
+
+(defn create-where
+  "Create the where conditions linking `pks` to `values.`
+  E.g.: id = 1 and name = 'test'"
+  [pks values]
+  (->> (map wrap-string values)
+       (map #(str %1 " = " %2) pks)
+       (interpose " and ")
+       (apply str)))
+
+(defn kebab-case->snake-case
+  "Transform the first element of `v` from kebab-case to snake_case."
+  [v]
+  (let [col (first v)
+        as (second v)]
+    (vector (db/kebab-case->snake-case col) as)))
+
+(defn format-as
+  "Format the elements in `v` as SQL query parameters.
+  E.g.: [test Test] => test as test
+        [test] => test"
+  [v]
+  (let [col (first v)
+        as (second v)]
+    (if as
+      (str col " as " as)
+      col)))
+
+(defn format-fields
+  "Transform `fs` in a format suitable for MySQL.
+  E.g.: id,text:Text => id, text as Text"
+  [fs]
+  (->> (string/split fs #",")
+       (map #(string/split % #":"))
+       (map kebab-case->snake-case)
+       (map format-as)
+       (interpose ", ")
+       (apply str)))
+
+(defn format-params
+  "Format `params` map as where conditions."
+  [params]
+  (let [ks (->> (keys params) (map name) (map db/kebab-case->snake-case))
+        vs (->> (vals params) (map wrap-string))]
+    (->> (map #(str %1 " = " %2) ks vs)
+         (interpose " and ")
+         (apply str))))
+
+(defn format-column-value
+  "Convert `value` in a format suitable for MySQL."
+  [schema table column value]
+  (if (number? value)
+    (-> (db/format-value schema table column (.longValue value))
+        (wrap-string))
+    (wrap-string value)))
+
+(defn params->mysql-params
+  "Convert `params` in a format suitable for MySQL."
+  [schema table params]
+  (reduce-kv
+   (fn [m k v]
+     (let [column (-> (name k) (db/kebab-case->snake-case))
+           value (format-column-value schema table column v)]
+       (assoc m column value)))
+   {}
+   params))
+
+(defn format-update-params
+  "Format `params` map as update set values."
+  [schema table params]
+  (let [mysql-params (params->mysql-params schema table params)
+        ks (keys mysql-params)
+        vs (vals mysql-params)]
+    (->> (map #(str %1 " = " %2) ks vs)
+         (interpose ",")
+         (apply str))))
---
+++ test/remys/api/resources/format_test.clj
@@ -0,0 +1,53 @@
+(ns remys.api.resources.format-test
+  (:require [remys.api.resources.format :as f]
+            [remys.services.mysql :as db]
+            [clojure.test :refer :all]))
+
+(def schema {"test" [{:column-name "test1"} {:column-name "test2"}]})
+
+(deftest wrap-string-test
+  (testing "Testing wrap-string resource"
+    (is (= "'test'" (f/wrap-string "test")))
+    (is (= 1 (f/wrap-string 1)))))
+
+(deftest create-where-test
+  (testing "Testing create-where resource"
+    (let [pks ["id" "text"]
+          values [1 "test"]]
+      (is (= (f/create-where pks values) "id = 1 and text = 'test'")))))
+
+(deftest kebab-case->snake-case-test
+  (testing "Testing kebab-case->snake-case resource"
+    (let [v ["test-col" "Test"]]
+      (is (= (f/kebab-case->snake-case v)
+             ["test_col" "Test"])))))
+
+(deftest format-as-test
+  (testing "Testing format-as resource"
+    (let [v ["test" "Test"]]
+      (is (= (f/format-as v)
+             "test as Test")))))
+
+(deftest format-fields-test
+  (testing "Testing format-fields resource"
+    (let [fs "id,text:Text"]
+      (is (= (f/format-fields fs)
+             "id, text as Text")))))
+
+(deftest format-params-test
+  (testing "Testing format-params resource"
+    (let [params {:id 1 :text "test"}]
+      (is (= (f/format-params params) "id = 1 and text = 'test'")))))
+
+(deftest format-column-value-test
+  (testing "Testing format-column-value resource"
+    (is (= (f/format-column-value schema "test" "test1" "text") "'text'"))
+    (is (= (f/format-column-value schema "test" "test1" 1) 1))))
+
+(deftest params->mysql-params-test
+  (testing "Testing params->mysql-params resource"
+    (is (= (f/params->mysql-params schema "test" {:test1 1}) {"test1" 1}))))
+
+(deftest format-update-params-test
+  (testing "Testing format-update-params resource"
+    (is (= (f/format-update-params schema "test" {:test1 1}) "test1 = 1"))))
